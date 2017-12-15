947f14710f98b04c6fd438e6d4106f0616db0825
Sasa33 <chengsasa33@gmail.com>
2017-12-15 21:26:13 +0800

    adjust styles

--- client/components/Home/recommend.js
+++ client/components/Home/recommend.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import FontAwesome from 'react-fontawesome';
+import { Flex } from 'antd-mobile';
 
 import './recommend.less';
 import Carousels from './carousels'
@@ -17,6 +19,28 @@ class Recommend extends Component {
     return (
       <div style={{ width: '100%' }}>
         <Carousels/>
+        <div className="recommend-container">
+          <Flex>
+            <Flex.Item>
+              <div>
+                <FontAwesome key="address-card-o" name="address-card-o"/>
+              </div>
+              <span>私人FM</span>
+            </Flex.Item>
+            <Flex.Item>
+              <div>
+                <FontAwesome key="calendar" name="calendar"/>
+              </div>
+              <span>每日歌曲推荐</span>
+            </Flex.Item>
+            <Flex.Item>
+              <div>
+                <FontAwesome key="signal" name="signal"/>
+              </div>
+              <span>云音乐热歌榜</span>
+            </Flex.Item>
+          </Flex>
+        </div>
       </div>
     )
   }
--- client/pages/home.js
+++ client/pages/home.js
@@ -1,13 +1,12 @@
 import React, { Component } from 'react';
-import { Tabs, Badge } from 'antd-mobile';
+import { Tabs } from 'antd-mobile';
 import LazyLoad from 'react-lazyload';
 import Header from './header'
 import { connect } from 'react-redux';
-import '../styles/home.less'
+import '../styles/home.less';
 
 import Recommend from '../components/Home/recommend';
 
-const TabPane = Tabs.TabPane;
 
 const tabs = [
   { title: "个性推荐" },
@@ -26,21 +25,21 @@ class Home extends Component {
       <div>
         <Header />
         <Tabs tabs={tabs}
-              initialPage={1}
+              initialPage={0}
               onChange={(tab, index) => { console.log('onChange', index, tab); }}
               onTabClick={(tab, index) => { console.log('onTabClick', index, tab); }}
         >
-          <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', height: '150px', backgroundColor: '#fff' }}>
+          <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', backgroundColor: '#fff' }}>
             <LazyLoad height={200}>
               <Recommend/>
             </LazyLoad>
           </div>
-          <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', height: '150px', backgroundColor: '#fff' }}>
+          <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', backgroundColor: '#fff' }}>
             <LazyLoad height={200}>
               <span>这是歌单页面</span>
             </LazyLoad>
           </div>
-          <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', height: '150px', backgroundColor: '#fff' }}>
+          <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', backgroundColor: '#fff' }}>
             <LazyLoad height={200}>
               <span>这是排行榜页面</span>
             </LazyLoad>
